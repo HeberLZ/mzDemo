22e855a553ff98a80fdc4d91d231c4adec98e082
HeberLZ <heberlopez@gmail.com>
2014-11-09 03:20:55 -0300

    run block used for debugging purposes removed

--- src/app/app.js
+++ src/app/app.js
@@ -5,11 +5,6 @@
   /*
    * Should only have view module dependencies or pluggable behaviour enhancer
    */
-  angular.module("mzDemo", ["mzResponsive", "mzMainpage"])
-      .run(function($rootScope){
-        $rootScope.$on("MEDIA_MODE_CHANGE", function(event, newMediaMode){
-          console.log(newMediaMode);
-        });
-      });
+  angular.module("mzDemo", ["mzResponsive", "mzMainpage"]);
 
 })();
